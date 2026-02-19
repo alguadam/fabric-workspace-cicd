679da8c8b9923fe815e4845fc491b5963fe6ca8f
Alvaro Guadamillas Herranz <alvaroguadamillas@gmail.com>
2026-02-19 11:52:56 +0100

    Committing 1 item from workspace 82249858-6617-405c-b4be-d40ea331378b

---
+++ workspace-udf/notebooks/bronze_to_silver/bronze_to_silver_finance_account-new.Notebook/notebook-content.py
@@ -0,0 +1,37 @@
+# Fabric notebook source
+
+# METADATA ********************
+
+# META {
+# META   "kernel_info": {
+# META     "name": "synapse_pyspark"
+# META   },
+# META   "dependencies": {
+# META     "lakehouse": {
+# META       "default_lakehouse": "6677c554-f58d-4371-81a4-c7b5a53648b7",
+# META       "default_lakehouse_name": "maag_bronze",
+# META       "default_lakehouse_workspace_id": "82249858-6617-405c-b4be-d40ea331378b",
+# META       "known_lakehouses": [
+# META         {
+# META           "id": "6677c554-f58d-4371-81a4-c7b5a53648b7"
+# META         },
+# META         {
+# META           "id": "63081e19-3045-4aa1-b19f-6425719fe664"
+# META         }
+# META       ]
+# META     }
+# META   }
+# META }
+
+# CELL ********************
+
+# Welcome to your new notebook
+# Type here in the cell editor to add code!
+
+
+# METADATA ********************
+
+# META {
+# META   "language": "python",
+# META   "language_group": "synapse_pyspark"
+# META }
