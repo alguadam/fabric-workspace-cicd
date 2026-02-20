57202784daa1dd33be3006201a22e9c83c6b2ee7
Alvaro Guadamillas Herranz <alvaroguadamillas@gmail.com>
2026-02-20 16:35:43 +0100

    Committing 1 item from workspace 82249858-6617-405c-b4be-d40ea331378b

--- workspace-udf/notebooks/bronze_to_silver/bronze_to_silver_finance_account.Notebook/notebook-content.py
+++ workspace-udf/notebooks/bronze_to_silver/bronze_to_silver_finance_account.Notebook/notebook-content.py
@@ -3,20 +3,17 @@
 # METADATA ********************
 
 # META {
-# META   "kernel_info": {
-# META     "name": "synapse_pyspark"
-# META   },
 # META   "dependencies": {
 # META     "lakehouse": {
 # META       "default_lakehouse": "6677c554-f58d-4371-81a4-c7b5a53648b7",
-# META       "default_lakehouse_name": "maag_bronze",
+# META       "default_lakehouse_name": "maag_silver",
 # META       "default_lakehouse_workspace_id": "82249858-6617-405c-b4be-d40ea331378b",
 # META       "known_lakehouses": [
 # META         {
-# META           "id": "63081e19-3045-4aa1-b19f-6425719fe664"
+# META           "id": "6677c554-f58d-4371-81a4-c7b5a53648b7"
 # META         },
 # META         {
-# META           "id": "6677c554-f58d-4371-81a4-c7b5a53648b7"
+# META           "id": "63081e19-3045-4aa1-b19f-6425719fe664"
 # META         }
 # META       ]
 # META     }
@@ -145,13 +142,6 @@ except Exception as e:
     raise
 # --- End Fabric Channel ---
 
-# METADATA ********************
-
-# META {
-# META   "language": "python",
-# META   "language_group": "synapse_pyspark"
-# META }
-
 # CELL ********************
 
 # # --- ADB Channel ---
@@ -250,10 +240,3 @@ except Exception as e:
 #     print(f"❌ Error loading data to table: {str(e)}")
 #     raise
 # # --- End ADB Channel ---
-
-# METADATA ********************
-
-# META {
-# META   "language": "python",
-# META   "language_group": "synapse_pyspark"
-# META }
